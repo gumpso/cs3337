898b17c8b3ca20c0555034500631bc1c7ae3a8bc
jin su <95654259+gumpso@users.noreply.github.com>
2023-11-16 02:18:04 -0800

    092023

--- bookMng/views.py
+++ bookMng/views.py
@@ -1,12 +1,18 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from .models import MainMenu
+
 # Create your views here.
 
 
 def index(request):
     #return HttpResponse("<h1> Hello </h1>")
     return render(request,
-                  'base.html')
+                  'bookMng/displaybooks.html',
+                  {
+                      'item_list': MainMenu.objects.all()
+                  }
+                  )
 
 
