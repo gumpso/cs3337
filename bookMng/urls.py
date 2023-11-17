49882fd6589b7a597c8cd3a877e399188eaab8c6
jin su <95654259+gumpso@users.noreply.github.com>
2023-11-16 23:09:38 -0800

    1108 final

--- bookMng/urls.py
+++ bookMng/urls.py
@@ -3,4 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('aboutus', views.aboutus, name='aboutus'),
+    path('book_detail/<int:book_id>', views.book_detail, name='book_detail'),
+    path('book_delete/<int:book_id>', views.book_delete, name='book_delete'),
+    path('postbook', views.postbook, name='postbook'),
+    path('displaybooks', views.displaybooks, name='displaybooks'),
+    path('mybooks', views.mybooks, name='mybooks'),
 ]
