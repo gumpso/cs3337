62d512dc68c40ed5d3b831b343134a6ab5ab25eb
anoymity <jtan@csula.edu>
2023-12-03 22:57:49 -0800

    deploy

--- bookEx/settings.py
+++ bookEx/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ggy=qy#=k)d3#ikmext4=h30u%8%-$u2)=cq0-)^nux*upkro0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [tjwl.pythonanywhere.com]
 
 
 # Application definition
